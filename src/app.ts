087cf60dad523061edfd065469810cc482258fdb
viniciuzrock <vinir.santoss@gmail.com>
2023-05-24 23:12:30 -0300

    WIP - importando arquivos

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,12 @@ import config from "config";
 import router from "./router";//routes
 import db from "../config/db";
 import Looger from "../config/logger";
+import morganMiddleware from "./middleware/morganMiddleware";
 const app = express()
 const port = config.get<number>('port');//app port
 
 app.use(express.json());//json middleware
-
+app.use(morganMiddleware)
 app.use("/api/", router)
 
 app.listen(port, async () => {
