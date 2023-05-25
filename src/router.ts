0b54e5a83f1b73c86f46251f02ebb96f8fc5f502
viniciuzrock <vinir.santoss@gmail.com>
2023-05-24 23:08:23 -0300

    WIP - adicionando router

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,11 @@
 import { Router, Request, Response } from "express";
+import { createMovie } from "./controllers/movieControllers";
+import { validate } from "./middleware/handleValidation";
+import { movieCreateValidation } from "./middleware/movieValidation";
+
 
 const router = Router()
 
 export default router.get("/test", (req: Request, res: Response) => {
     res.status(200).send('API ok')
-})
+}).post('/movie', movieCreateValidation(), validate, createMovie)
