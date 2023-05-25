73bd90850f3351b40ea7ce3d3ea4b5cf0ffa846a
viniciuzrock <vinir.santoss@gmail.com>
2023-05-25 16:47:36 -0300

    WIP - adicionando resgate individual e geral de movies

--- src/controllers/movieControllers.ts
+++ src/controllers/movieControllers.ts
@@ -12,3 +12,31 @@ export async function createMovie(req: Request, res: Response) {
         Logger.error(`Erro no sistema: ${error.message}`)
     }
 }
+
+//create function for find movie by id
+export async function findMovieById(req: Request, res: Response) {
+    try {
+        const id = req.params.id
+        const movie = await MovieModel.findById(id)
+        if (!movie) {
+            return res.status(404).json({ message: "Filme não encontrado" })
+        }
+        return res.status(200).json(movie)
+    } catch (e: any) {
+        Logger.error(`Erro no sistema: ${e.message}`)
+    }
+}
+
+export async function getAllMovies(req: Request, res: Response) {
+    try {
+        const movies = await MovieModel.find()
+        if (!movies) {
+            return res.status(404).json({
+                message: "Sem filmes disponíveis"
+            })
+        }
+        return res.status(200).json(movies)
+    } catch (e: any) {
+        Logger.error(`Erro no sistema: ${e.message}`)
+    }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,21 @@
 import { Router, Request, Response } from "express";
-import { createMovie } from "./controllers/movieControllers";
+import { createMovie, findMovieById, getAllMovies } from "./controllers/movieControllers";
 import { validate } from "./middleware/handleValidation";
 import { movieCreateValidation } from "./middleware/movieValidation";
 
 
 const router = Router()
 
-export default router.get("/test", (req: Request, res: Response) => {
-    res.status(200).send('API ok')
-}).post('/movie', movieCreateValidation(), validate, createMovie)
+export default router
+    .get("/test", (req: Request, res: Response) => {
+        res.status(200).send('API ok')
+    })
+    .post('/movie', movieCreateValidation(), validate, createMovie)
+    .get('/movie/:id', findMovieById)
+    .get('/movie', getAllMovies)
+
+
+
+
+
+
